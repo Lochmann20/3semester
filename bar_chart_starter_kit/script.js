88ccf2c336ac98eb10341fd87c5f89139fae714b
Lochmann20 <Linemoller1907@gmail.com>
2024-02-05 12:01:44 +0100

    ayyays

---
+++ bar_chart_starter_kit/script.js
@@ -0,0 +1,39 @@
+const list = document.querySelector("ul");
+const li = document.createElement("li");
+
+li.style.setProperty("--height", "50");
+list.appendChild(li);
+
+
+
+
+
+
+
+
+// let myArray = [];
+// setInterval(function() {
+//     myArray.push(Math.floor(Math.random() * 100));
+//     // console.log(myArray);
+
+//     let arrayLength = myArray.length;
+
+//      if(arrayLength > 5) {
+//         myArray.shift();
+//      }
+//      console.log("", + arrayLength);
+//     }, 1000);
+
+
+// shift
+
+// arrayLength
+
+
+
+
+
+
+
+
+
